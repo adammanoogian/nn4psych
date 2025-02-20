5b8aa24f84d07947fe01f2a60a48b5b88a7a39d7
a-manoogian <adammanoogian@gmail.com>
2025-02-20 11:56:30 +1100

    added sem

--- nassarfig6.py
+++ nassarfig6.py
@@ -47,6 +47,9 @@ sd_mod_odd_pat = np.std(mod_data[0][0][ob][:sz_pat][:][:], axis=0)
 sd_mod_cp_control = np.std(mod_data[0][0][cp][sz_pat:][:][:], axis=0)
 sd_mod_odd_control = np.std(mod_data[0][0][ob][sz_pat:][:][:], axis=0)
 
+
+#%% calcs
+
 #calc the differences line
 diff_sub_pat = np.mean(np.array([mean_sub_cp_pat[:,1], mean_sub_odd_pat[:,1]]), axis=0)
 diff_sub_control = np.mean(np.array([mean_sub_cp_control[:,1], mean_sub_odd_control[:,1]]), axis=0)
@@ -60,50 +63,74 @@ area_sub_control = np.trapz(np.abs(mean_sub_cp_control[:,1] - mean_sub_odd_contr
 area_mod_pat = np.trapz(np.abs(mean_mod_cp_pat[:,1] - mean_mod_odd_pat[:,1]), dx=1)
 area_mod_control = np.trapz(np.abs(mean_mod_cp_control[:,1] - mean_mod_odd_control[:,1]), dx=1)
 
+#standard error of rad ( cp^2 + ob^2)
+sem_sub_pat = np.sqrt((sd_sub_cp_pat[:,1]**2 + sd_sub_odd_pat[:,1]**2) / sz_pat)
+sem_sub_control = np.sqrt((sd_sub_cp_control[:,1]**2 + sd_sub_odd_control[:,1]**2) / (len(sub_data[0][0][cp]) - sz_pat))
+
 print("Subject Patients Total Difference:", area_sub_pat)
 print("Subject Controls Total Difference:", area_sub_control)
 print("Model Patients Total Difference:", area_mod_pat)
 print("Model Controls Total Difference:", area_mod_control)
 
+print("Subject Patients SEM:", sem_sub_pat)
+print("Subject Controls SEM:", sem_sub_control)
 
 #%%plots
-
-x_axis = [i for i in range(115)]
 
 #TO-DO: add error bars (right now, only controls have error bars representative of the paper figure, so must have been some cleaning on SZ group)
 def plot_fig6a_no_error(diff_score):
     plt.figure(figsize=(10, 6))
     plt.ylim(0, 1)
-    plt.plot(x_axis, mean_sub_cp_pat[:,1], label='CP pat', color='orange')
-    plt.plot(x_axis, mean_sub_odd_pat[:,1], label='OB pat', color='brown')
-    plt.plot(x_axis, mean_sub_cp_control[:,1], label='CP control', color='blue')
-    plt.plot(x_axis, mean_sub_odd_control[:,1], label='OB control', color='green')
+    plt.plot(x_axis, mean_sub_cp_pat[:,1], label='sub CP pat', color='orange')
+    plt.plot(x_axis, mean_sub_odd_pat[:,1], label='sub OB pat', color='brown')
+    plt.plot(x_axis, mean_sub_cp_control[:,1], label='sub CP control', color='blue')
+    plt.plot(x_axis, mean_sub_odd_control[:,1], label='sub OB control', color='green')
+    
+    
     if diff_score:
-        plt.plot(x_axis, diff_sub_pat, label='CP-OB pat', color='red')
-        plt.plot(x_axis, diff_sub_control, label='CP-OB control', color='purple')
+        plt.plot(x_axis, diff_sub_pat, label='sub CP-OB pat', color='red')
+        plt.plot(x_axis, diff_sub_control, label='sub CP-OB control', color='purple')
     plt.xlabel('Relative Error')
     plt.ylabel('Learning Rate')
     plt.legend()
-    plt.savefig('nassarfig6a.png')
+    plt.savefig('plots/nassarfig6a.png')
     plt.show()
 
 
 def plot_fig6b(diff_score):
     plt.figure(figsize=(10, 6))
     plt.ylim(0, 1)
-    plt.plot(x_axis, mean_mod_cp_pat[:,1], label='CP pat', color='orange')
-    plt.plot(x_axis, mean_mod_odd_pat[:,1], label='OB pat', color='brown')
-    plt.plot(x_axis, mean_mod_cp_control[:,1], label='CP control', color='blue')
-    plt.plot(x_axis, mean_mod_odd_control[:,1], label='OB control', color='green')
+    plt.plot(x_axis, mean_mod_cp_pat[:,1], label='mod CP pat', color='orange')
+    plt.plot(x_axis, mean_mod_odd_pat[:,1], label='mod OB pat', color='brown')
+    plt.plot(x_axis, mean_mod_cp_control[:,1], label='mod CP control', color='blue')
+    plt.plot(x_axis, mean_mod_odd_control[:,1], label='mod OB control', color='green')
     if diff_score:
-        plt.plot(x_axis, diff_mod_pat, label='CP-OB pat', color='red')
-        plt.plot(x_axis, diff_mod_control, label='CP-OB control', color='purple')
+        plt.plot(x_axis, diff_mod_pat, label='mod CP-OB pat', color='red')
+        plt.plot(x_axis, diff_mod_control, label='mod CP-OB control', color='purple')
     plt.xlabel('Relative Error')
     plt.ylabel('Learning Rate')
     plt.legend()
-    plt.savefig('nassarfig6b.png')
+    plt.savefig('plots/nassarfig6b.png')
     plt.show()
+
+def plot_bar_graph_area_between_curves():
+    plt.figure(figsize=(10, 6))
+    plt.bar(['Sub pat', 'Sub con', 'Mod pat', 'Mod con'], [area_sub_pat, area_sub_control, area_mod_pat, area_mod_control], color=['orange', 'brown', 'blue', 'green'])
+    plt.ylabel('Area between curves')
+    plt.title('Area between CP and OB learning rate curves')
+    plt.savefig('plots/area_between_curves.png')
+    plt.show()
+
+
+
+#decide cutoff on x-axis boundaries ("relativity" in relative error)
+x_axis = [i for i in range(115)]
+
+
 
 
 plot_fig6a_no_error(diff_score = True)
 plot_fig6b(diff_score = True)
+plot_bar_graph_area_between_curves()
+
+
